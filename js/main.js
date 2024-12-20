6b470bf06c7bdd9694d69804a15c3fcc8f7e63c8
mog147 <396mog@gmail.com>
2024-12-20 21:21:02 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: true,//下部ドットナビゲーションの表示
+});
+
+
+$('.slide__full').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+    speed:1000,//スライドの動きのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: true,//左右の矢印あり
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slide__full').on('touchmove', function(event, slick, currentSlide, nextSlide){
+$('.slide__full').slick('slickPlay');
+});
+
+// hamburger
+$(function(){
+    $(".btn-gNav").on("click",function(){
+       $(".gnav").toggleClass("open");
+    });
+  });
+
+
+// 
+
+
+$(function(){
+
+    //表示非表示切替
+    $(".more-btn").on("click",function(){
+
+        if ($('#tgt').hasClass('open')){
+
+            // 非表示の場合の処理
+            $(".conF").css({'padding-bottom':'0rem'});
+            $('#tgt').removeClass('open');
+
+        } else {
+
+            // 表示の場合の処理
+            $('#tgt').addClass('open');
+            $(".conF").css({'padding-bottom':'50rem'});
+    
+        }
+    })
+    
+});
+
